e914481dc6b2717d1527620b5d297c4c597f48b7
shulgup <shulbhi1810@gmail.com>
2023-12-15 18:26:57 +0530

    updated

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom";
 import { useTitle } from "../hooks/useTitle";
+import { Link } from "react-router-dom";
 
 export const PageNotFound = () => {
   useTitle("Page Not Found");
